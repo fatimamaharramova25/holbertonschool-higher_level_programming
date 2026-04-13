1ff90bbcc886239fe0fd5ee2a988820365874c6e
Fatima Maharramova <fatimamaharramova26@gmail.com>
2026-04-13 20:09:41 +0000

    Fix permission and logic for task 6

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-str = "object-oriented programming with Python"
-print(str[0:6] + str[9:12] + str[14:30] + str[31:40])
+str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
+str = str[39:67] + str[106:112] + str[:6]
+print(str)
