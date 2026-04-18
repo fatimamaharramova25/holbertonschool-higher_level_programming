5477cf7f90852a368f6c95b9449d812942b0ecd9
fatimamaharramova25 <sfatimamaharramova26@gmail.com>
2026-04-18 17:42:30 +0000

    Fix E501: line too long by splitting variables

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
+""" Roman to Integer module """
+
+
 def roman_to_int(roman_string):
+    """ Converts a Roman numeral to an integer """
     if not isinstance(roman_string, str) or roman_string is None:
         return 0
 
-    roman_dict = {
-        'I': 1, 'V': 5, 'X': 10, 'L': 50,
-        'C': 100, 'D': 500, 'M': 1000
-    }
+    rd = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     num = 0
     for i in range(len(roman_string)):
-        current = roman_dict.get(roman_string[i], 0)
-        if i + 1 < len(roman_string) and roman_dict.get(roman_string[i + 1], 0) > current:
+        current = rd.get(roman_string[i], 0)
+        next_v = rd.get(roman_string[i + 1], 0) if i + 1 < len(roman_string)             else 0
+        if next_v > current:
             num -= current
         else:
             num += current
