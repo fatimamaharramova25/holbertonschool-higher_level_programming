f085fc7444590a66d19fb9573d26864cb74bc5c8
fatimamaharramova25 <fatimamaharramova26@gmail.com>
2026-04-18 13:48:59 +0000

    Add infinite addition script for arguments

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -3,7 +3,7 @@ import sys
 
 if __name__ == "__main__":
     total = 0
-    # sys.argv[0] proqramın adıdır, onu keçib 1-dən sona qədər hamısını toplayırıq
-    for i in range(1, len(sys.argv)):
-        total += int(sys.argv[i])
+    # sys.argv[0] skriptin adıdır, ona görə 1-ci indeksdən başlayırıq
+    for arg in sys.argv[1:]:
+        total += int(arg)
     print("{}".format(total))
